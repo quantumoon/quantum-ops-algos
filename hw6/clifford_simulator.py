f024cf055396baf8607474879542d588fdca5b9a
Daniil Bagaev <bagaev_daniel@list.ru>
2025-04-21 01:44:23 +0300

    Add measure method for simulator

--- hw6/clifford_simulator.py
+++ hw6/clifford_simulator.py
@@ -44,3 +44,34 @@ class CliffordSimulator:
                 op_str += pauli_map[(x, z)]
             stabilizers.append(sign + op_str)
         return stabilizers
+
+    def measure(self, q):
+        # If there are any X stabilizers, then measurement is not deterministic
+        # Otherwise it is
+        anti_row = None
+        for r in range(self.n, 2 * self.n):
+            if self.tableau[r, q] == 1:
+                anti_row = r
+                break
+        
+        # Deterministic
+        if anti_row is None:
+            for r in range(self.n, 2 * self.n):
+                if self.tableau[r, q] == 0 and self.tableau[r, self.n + q] == 1:
+                    return int(self.tableau[r, 2 * self.n])
+            return 0
+        
+        # Not deterministic
+        result = np.random.randint(2)
+        for r in range(2 * self.n):
+            if r != anti_row and self.tableau[r, q] == 1:
+                self.tableau[r, :] ^= self.tableau[anti_row, :]
+        
+        self.tableau[anti_row, :] = 0
+        self.tableau[anti_row, self.n + q] = 1
+        self.tableau[anti_row, 2 * self.n] = result
+        
+        self.tableau[q, :] = 0
+        self.tableau[q, q] = 1
+        self.tableau[q, 2 * self.n] = 0
+        return result
